1cf91f311fa0f621839db9d718be183a7ca04a73
Luis Torres <luistjr@gmail.com>
2020-09-24 20:35:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  chocolate.push()
+  chocolate.push("mush")
+  p chocolate
 end
 
 def add_element_to_start_of_array(array, element)

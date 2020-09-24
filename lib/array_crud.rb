23f813bc8e161441c3209700cc4aa7c026081223
Luis Torres <luistjr@gmail.com>
2020-09-24 20:37:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("element")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)

080b0a4893bbeac150f9e77df8a7b07cb176adb5
Luis Torres <luistjr@gmail.com>
2020-09-24 20:33:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,15 @@
 def create_an_empty_array
   chocolate = []
-  
 end
 
 def create_an_array
   
+  chocolate = ["ice cream", "cake", "muffins", "rice"]
+  
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  chocolate.push("bar")
 end
 
 def add_element_to_start_of_array(array, element)
